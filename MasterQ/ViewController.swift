aefdf6030b41a5daed98667a085cc8ecde3283f2
Ben St. John <ben@bens-mbp.attlocal.net>
2018-12-17 13:31:00 -0500

    Initial Commit

---
+++ MasterQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterQ
+//
+//  Created by Ben St. John on 12/17/18.
+//  Copyright © 2018 Ben St. John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
